277c653c524c2921d24c060d8ce0abac8947a624
jvandertil <jvandertil@gmail.com>
2011-01-07 14:51:10 +0100

    Basic Adjacency List Structure

---
+++ src/nl/rug/gad/practicum2/Edge.java
@@ -0,0 +1,8 @@
+package nl.rug.gad.practicum2;
+
+public class Edge {
+
+	public int capacity;
+	public Vertex start;
+	public Vertex end;
+}
---
+++ src/nl/rug/gad/practicum2/Graph.java
@@ -0,0 +1,18 @@
+package nl.rug.gad.practicum2;
+
+import java.util.List;
+
+public class Graph {
+
+	public List<Edge> edgeList;
+	public List<Vertex> vertexList;
+	
+	//insert(Vertex)
+	//insert(Edge e, Vertex start, Vertex eind)
+	//areAdjacent(Vertex a, Vertex b)
+	
+	//Optioneel:
+	//RemoveVertex
+	//RemoveEdge
+	
+}
