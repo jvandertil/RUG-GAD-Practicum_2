41f0b5339b4ad80d17cb4a24261bcf6799b9ce0d
Jos van der Til <jvandertil@gmail.com>
2011-01-18 22:13:38 +0100

    Breadth First Search JONGUH

--- src/nl/rug/gad/practicum2/AugmentingPath.java
+++ src/nl/rug/gad/practicum2/AugmentingPath.java
@@ -1,8 +1,9 @@
 package nl.rug.gad.practicum2;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 import nl.rug.gad.practicum2.Edge.EdgeStatus;
@@ -11,24 +12,26 @@ import nl.rug.gad.practicum2.Vertex.VertexStatus;
 public class AugmentingPath {
 
 	private static boolean stop = false;
-	
-	public static List<Edge> getAugmentedPathDFS(Graph g, Vertex s, Vertex t, List<Edge> path){
+
+	public static List<Edge> getAugmentedPathDFS(Graph g, Vertex s, Vertex t,
+			List<Edge> path) {
 		stop = false;
 		List<Edge> unionEdge = s.getAllEdges();
-		
+
 		s.status = VertexStatus.EXPLORED;
-		for(Edge e : unionEdge) {
-			if(e.status == EdgeStatus.UNEXPLORED && getResidualCapacity(s, e) > 0){
+		for (Edge e : unionEdge) {
+			if (e.status == EdgeStatus.UNEXPLORED
+					&& getResidualCapacity(s, e) > 0) {
 				Vertex w = g.opposite(s, e);
-				if(w.status == VertexStatus.UNEXPLORED && canContinue(w, e, t)){
+				if (w.status == VertexStatus.UNEXPLORED && canContinue(w, e, t)) {
 					e.status = EdgeStatus.DISCOVERY;
 					path.add(e);
-					if(e.start.equals(s)){
+					if (e.start.equals(s)) {
 						e.forward = true;
 					} else {
 						e.forward = false;
 					}
-					if(w.equals(t)){
+					if (w.equals(t)) {
 						stop = true;
 						return path;
 					}
@@ -38,37 +41,20 @@ public class AugmentingPath {
 					path.remove(e);
 				}
 			}
-			if(stop){
+			if (stop) {
 				return path;
 			}
 		}
 		return path;
-	}
-	
-	private static boolean canContinue(Vertex v, Edge source, Vertex destination){
-		for(Edge e : v.getAllEdges()){
-			if(v.equals(destination)){
-				return true;
-			}
-			if(getResidualCapacity(v, e) > 0 && !e.equals(source)){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private static int getResidualCapacity(Vertex v, Edge e){
-		if(e.start.equals(v)){
-			return e.capacity - e.flow;
-		} else {
-			return e.flow;
-		}
 	}
 
 	public static List<Edge> getAugmentedPathBFS(Graph g, Vertex s, Vertex t) {
 		List<Edge> path = new LinkedList<Edge>();
 		Queue<Vertex> vertexQueue = new LinkedList<Vertex>();
 		
+		//Map <x,y>. Vertex y discovered via x
+		Map<Vertex, Edge> discoveryMap = new HashMap<Vertex, Edge>(); 
+
 		s.status = VertexStatus.EXPLORED;
 		vertexQueue.add(s);
 
@@ -77,27 +63,66 @@ public class AugmentingPath {
 
 		while (!vertexQueue.isEmpty()) {
 			w = vertexQueue.poll();
-			
-			unionEdge = new LinkedList<Edge>();
-			
-			unionEdge.addAll(w.incomingEdges);
-			unionEdge.addAll(w.outgoingEdges);
-			
-			for(Edge e : unionEdge) {
-				Vertex next = e.end;
-				if(next.status == VertexStatus.UNEXPLORED && e.hasResidualCapacity()) {
-					vertexQueue.add(next);
-					next.status = VertexStatus.EXPLORED;
-					path.add(e);
-					
-					if(next == t)
-						return path;
+
+			unionEdge = w.getAllEdges();
+
+			for (Edge e : unionEdge) {
+				if (e.status == EdgeStatus.UNEXPLORED
+						&& (getResidualCapacity(w, e) > 0)) {
+					Vertex next = e.end;
+					if (next.status == VertexStatus.UNEXPLORED) {
+						vertexQueue.add(next);
+						discoveryMap.put(next, e);
+						next.status = VertexStatus.EXPLORED;
+						e.status = EdgeStatus.DISCOVERY;
+
+						if (next == t) {
+							//sink found. Trace back.
+							boolean traceDone = false;
+							Edge currentEdge;
+							Vertex previousVertex = next;
+							while(!traceDone) {
+								currentEdge = discoveryMap.get(previousVertex);
+								path.add(currentEdge);
+								
+								previousVertex = currentEdge.start;
+								
+								if(previousVertex == s)
+									traceDone = true;
+							}
+							
+							return path;
+						}
+					} else {
+						e.status = EdgeStatus.BACK;
+						e.forward = false;
+					}
 				}
 			}
-			
+
 			unionEdge = null;
 		}
-		
+
 		return path;
+	}
+
+	private static boolean canContinue(Vertex v, Edge source, Vertex destination) {
+		for (Edge e : v.getAllEdges()) {
+			if (v.equals(destination)) {
+				return true;
+			}
+			if (getResidualCapacity(v, e) > 0 && !e.equals(source)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static int getResidualCapacity(Vertex v, Edge e) {
+		if (e.start.equals(v)) {
+			return e.capacity - e.flow;
+		} else {
+			return e.flow;
+		}
 	}
 }
